74faaa796f293b71c3363c24587212fc49e28712
David Hin <hello@davidhin.com>
2021-12-09 11:47:17 +1030

    Minor updates

--- jpsentences/main.py
+++ jpsentences/main.py
@@ -14,5 +14,10 @@ df["japanese"] = df.japanese.apply(wk.furigana)
 # Move level column to last
 df = df[[c for c in df.columns if c != "level"] + ["level"]]
 
+# Grammar rules to consecutive divs
+df.grammar = df.grammar.apply(
+    lambda x: "".join([f"<div>{i}</div>" for j in x.items() for i in j])
+)
+
 # Save CSV
-df.to_csv(jp.outputs_dir() / "wk.csv", index=0)
+df.to_csv(jp.outputs_dir() / "wk.csv", index=0, header=None)
